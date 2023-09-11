82af7eee7f7910be667667cd4b8697fe4f3fd9ce
Leo Rodriguez <leorod18@yahoo.com>
2023-09-11 00:24:22 -0400

    remove more import error

--- src/pages/projects/huddln.tsx
+++ src/pages/projects/huddln.tsx
@@ -4,9 +4,6 @@ import Section from '@/components/Section'
 import Article from '@/components/layouts/Article'
 import { Badge, Container, List, Link, ListItem, UnorderedList, SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
-// import Huddln_1 from "/mp4/huddln_1.mp4"
-// import Huddln_1 from "../../../public/mp4/huddln_1.mp4"
-import Huddln_1 from "../../../public/mp4/huddln_1.mp4";
 
 export default function Huddln() {
   return (
