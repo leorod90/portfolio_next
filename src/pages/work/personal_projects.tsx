3215c6dfdf5bfe2c8e0b5375105a9c7fd868e195
Leo Rodriguez <leorod18@yahoo.com>
2024-06-08 02:31:19 -0400

    add leo weather

--- src/pages/work/personal_projects.tsx
+++ src/pages/work/personal_projects.tsx
@@ -21,7 +21,7 @@ export default function PersonalProjects() {
         <UnorderedList my={4}>
           <ListItem>Leo Eats - I enjoy cooking so I wanted to create an app that could store
             all of my recipes.</ListItem>
-          <ListItem>Leo Marvel - This is one of the first React Native apps I have built.</ListItem>
+          <ListItem>Leo Weather - This is one of the first React Native apps I built.</ListItem>
           <ListItem>My Avatar - I will make him dance!!!</ListItem>
         </UnorderedList>
 
@@ -44,7 +44,8 @@ export default function PersonalProjects() {
           </ListItem>
         </List>
         <Text fontWeight='bold' color='gray' mt={6}>Oops! My android account was deleted due to inactivity,
-          please bare wih me while I fix the issue.</Text>
+        and they also made it harder for solo developers to upload.
+          Please bare wih me while I fix the issue.</Text>
         <Text fontWeight='bold' color='glassTeal' mt={6}>Click Videos to Pause/Play</Text>
         <SimpleGrid
           justifyItems="center"
@@ -132,7 +133,84 @@ export default function PersonalProjects() {
 
         {/* <Text fontWeight='bold' color='glassTeal' mt={6}>Click Videos to Pause/Play</Text> */}
 
+
         <SimpleGrid justifyItems='center' columns={[1, 1, 2]} gap={1}>
+          <Flex
+            w="100%"
+            height="100%"
+            justifyContent='center'
+            alignItems='center'
+            order={[1, 1, 2]}>
+            <Flex
+              direction='column'
+              justifyContent='center'
+              alignItems='center'
+              my='15px'
+              bg='#413835'
+              px='5px'
+              py='15'
+              borderRadius='5px'>
+              <Image
+                width={50}
+                height={50}
+                src='/photos/leo_weather.png'
+                alt='app'
+                // className='grid-item-thumbnail'
+                // placeholder='blur'
+                loading='lazy'
+              />
+              <Flex justifyContent='space-between' width='320px' mt='15px'>
+                <Link href='https://apps.apple.com/us/app/leo-weather/id1602629978' rel="noopener noreferrer" target='_blank'>
+                  <Image
+                    width={150}
+                    height={60}
+                    src='/photos/app_store.png'
+                    alt='apple'
+                    // className='grid-item-thumbnail'
+                    // placeholder='blur'
+                    loading='lazy'
+                  />
+                </Link>
+                <Box
+                  filter="grayscale(100%)"
+                  cursor='pointer'
+                >
+                  {/* <Link href='https://play.google.com/store/apps/details?id=expo.leorod18.good_eats' rel="noopener noreferrer" target='_blank'> */}
+                  <Image
+                    width={150}
+                    height={60}
+                    src='/photos/google_play.png'
+                    alt='android'
+                    // className='grid-item-thumbnail'
+                    // placeholder='blur'
+                    loading='lazy'
+                  />
+                  {/* </Link> */}
+                </Box>
+              </Flex>
+              <Link mt='10px' href='https://github.com/leorod90/leo-weather' rel="noopener noreferrer" target='_blank'>
+                <Image
+                  width={150}
+                  height={60}
+                  src='/photos/github.png'
+                  alt='github'
+                  // className='grid-item-thumbnail'
+                  // placeholder='blur'
+                  loading='lazy'
+                />
+              </Link>
+            </Flex>
+          </Flex>
+          <SimpleGrid justifyItems='center' columns={[1, 2, 2]} gap={1} order={[2, 2, 1]}>
+            {/* <Flex direction='column' alignItems='center' gap={1} mt={3}> */}
+            <ProjectVideoPlayer src={require('../../../public/mp4/weather_1.mp4')} />
+            <ProjectVideoPlayer src={require('../../../public/mp4/weather_2.mp4')} />
+            {/* </Flex> */}
+          </SimpleGrid>
+        </SimpleGrid>
+
+
+        {/* <SimpleGrid justifyItems='center' columns={[1, 1, 2]} gap={1}>
           <Flex
             w="100%"
             height="100%"
@@ -153,8 +231,6 @@ export default function PersonalProjects() {
                 height={50}
                 src='/photos/leo_marvel.png'
                 alt='app'
-                // className='grid-item-thumbnail'
-                // placeholder='blur'
                 loading='lazy'
               />
               <Flex justifyContent='flex-end' width='320px' mt='15px'>
@@ -162,17 +238,13 @@ export default function PersonalProjects() {
                   filter="grayscale(100%)"
                   cursor='pointer'
                 >
-                  {/* <Link href='https://play.google.com/store/apps/details?id=com.leo_marvel' rel="noopener noreferrer" target='_blank'> */}
                   <Image
                     width={150}
                     height={60}
                     src='/photos/google_play.png'
                     alt='android'
-                    // className='grid-item-thumbnail'
-                    // placeholder='blur'
                     loading='lazy'
                   />
-                  {/* </Link> */}
                 </Box>
               </Flex>
               <Link mt='10px' href='https://github.com/leorod90/leo_marvel' rel="noopener noreferrer" target='_blank'>
@@ -181,20 +253,16 @@ export default function PersonalProjects() {
                   height={60}
                   src='/photos/github.png'
                   alt='git'
-                  // className='grid-item-thumbnail'
-                  // placeholder='blur'
                   loading='lazy'
                 />
               </Link>
             </Flex>
           </Flex>
           <SimpleGrid justifyItems='center' columns={[1, 2, 2]} gap={1} order={[2, 2, 1]}>
-            {/* <Flex direction='column' alignItems='center' gap={1} mt={3}> */}
             <ProjectVideoPlayer src={require('../../../public/mp4/marvel_1.mp4')} />
             <ProjectVideoPlayer src={require('../../../public/mp4/marvel_2.mp4')} />
-            {/* </Flex> */}
           </SimpleGrid>
-        </SimpleGrid>
+        </SimpleGrid> */}
 
 
         <Text my={3} fontWeight='bold' color='glassTeal' mt={6}>Bonus Video</Text>
